5bf24629b76c32adb1b22c2373655d44ecae6d39
JDaniel Jiménez Paniagua <jdjp83@gmail.com>
2016-06-21 23:13:29 +0200

    Added arg parse to control these vars from commandline: debug output, logfile location, path to pictures to upload, extension to mark files as uploaded and the path to the config file

--- flickup.py
+++ flickup.py
@@ -8,69 +8,133 @@ import re
 import datetime
 import sys
 import configparser
+import argparse
 
-import smtplib, os
+import smtplib
+import os
 from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email.mime.text import MIMEText
 from email.utils import COMMASPACE, formatdate
 from email import encoders
 
-config_file = "/etc/flickrup.conf"
+
+# Default vars
+default_args_config = '/etc/flickrup.conf'
+default_log_file = 'flickrup.log'
+default_uploaded_extension = 'done'
+
+# Initializing argparser
+parser = argparse.ArgumentParser(description='Flickrup: Flicker uploader.')
+
+# Enable debug
+parser.add_argument('-d',
+                    '--debug',
+                    help='Debug output. Default: Disabled',
+                    action='store_true'
+                    )
+
+# Path to config file
+parser.add_argument('-c',
+                    '--config',
+                    help='Path to config file. Defaul: {}'.format(
+                        default_args_config),
+                    required=False,
+                    default=default_args_config
+                    )
+
+# Path to dir with pictures
+parser.add_argument('-p',
+                    '--pictures',
+                    help='Path to dir with pictures to upload',
+                    required=False,
+                    default='/etc/flickrup.conf'
+                    )
+
+# Path to log file
+parser.add_argument('-l',
+                    '--log-file',
+                    help='Path to log file. Default: {}'.format(
+                        default_log_file),
+                    required=False,
+                    default=default_log_file
+                    )
+
+# Extension to mark files as uploaded
+parser.add_argument('-e',
+                    '--uploaded-extension',
+                    help='Extension to mark files as uploaded. Default: {}'.format(
+                        default_uploaded_extension),
+                    required=False,
+                    default=default_uploaded_extension
+                    )
+
+
+args = parser.parse_args()
+
+# Print resume
+if args.debug:
+    print("Running with these config options:")
+    for arg in vars(args):
+        print("\t- {}: {}".format(arg, getattr(args, arg)))
+
+
 config = configparser.ConfigParser()
 try:
-    config.read(config_file)
-    api_key = config.get("main","api_key")
-    api_secret = config.get("main","api_secret")
-    api_token = config.get("main","api_token")
-    email_from = config.get("main","email_from")
-    email_to = config.get("main","email_to")
+    config.read(args.config)
+    api_key = config.get("main", "api_key")
+    api_secret = config.get("main", "api_secret")
+    api_token = config.get("main", "api_token")
+    email_from = config.get("main", "email_from")
+    email_to = config.get("main", "email_to")
 except:
-  print("Missing "+config_file)
-  sys.exit(0)
+    print("Missing " + args.config)
+    sys.exit(0)
 
 
-done = ".done"
-
-sdcard = sys.argv[1] + "/"
-
-logfile = sdcard + "upload-log.txt"
-
-includes = ['*.jpg'] # for files only / case insensitive
-excludes = ["*"+done,logfile] # for dirs and files / case insensitive
+includes = ['*.jpg']  # for files only / case insensitive
+# for dirs and files / case insensitive
+excludes = ["*." + args.uploaded_extension, args.log_file]
 
 flickr = flickrapi.FlickrAPI(api_key, api_secret, api_token)
 
-def send_mail( send_from, send_to, subject, text, files=[], server="localhost", port=25, username='', password='', isTls=True):
+
+def send_mail(send_from, send_to, subject, text, files=[], server="localhost", port=25, username='', password='', isTls=True):
     msg = MIMEMultipart()
     msg['From'] = send_from
     msg['To'] = COMMASPACE.join(send_to)
-    msg['Date'] = formatdate(localtime = True)
+    msg['Date'] = formatdate(localtime=True)
     msg['Subject'] = subject
 
-    msg.attach( MIMEText(text) )
+    msg.attach(MIMEText(text))
 
     for f in files:
         part = MIMEBase('application', "octet-stream")
-        part.set_payload( open(f,"rb").read() )
+        part.set_payload(open(f, "rb").read())
         encoders.encode_base64(part)
-        part.add_header('Content-Disposition', 'attachment; filename="{0}"'.format(os.path.basename(f)))
+        part.add_header('Content-Disposition',
+                        'attachment; filename="{0}"'.format(os.path.basename(f)))
         msg.attach(part)
 
     smtp = smtplib.SMTP(server, port)
-    if isTls: smtp.starttls()
-    if len(username)>0 : smtp.login(username,password)
+    if isTls:
+        smtp.starttls()
+    if len(username) > 0:
+        smtp.login(username, password)
     smtp.sendmail(send_from, send_to, msg.as_string())
     smtp.quit()
 
-def logit( text ):
-    log.write(str(datetime.datetime.utcnow())+ " "+text+"\n")
+
+def logit(text):
+    log.write(str(datetime.datetime.utcnow()) + " " + text + "\n")
+
 
 def cleanexit():
 
     log.close()
 
-    send_mail( email_from, [email_to], "Photo Upload", "Photo upload log", [logfile])
+    send_mail(email_from, [email_to], "Photo Upload",
+              "Photo upload log", [args.log_file])
 
     sys.exit()
 
@@ -83,7 +147,7 @@ if not flickr.token_valid(perms='write'):
     # Open a browser at the authentication URL. Do this however
     # you want, as long as the user visits that URL.
     authorize_url = flickr.auth_url(perms='write')
-    print("Open the following URL on your web browser and copy the code to " + config_file)
+    print("Open the following URL on your web browser and copy the code to " + args.config)
     print(authorize_url)
 
     # Get the verifier code from the user. Do this however you
@@ -99,17 +163,17 @@ includes = r'|'.join([fnmatch.translate(x) for x in includes])
 excludes = r'|'.join([fnmatch.translate(x) for x in excludes]) or r'$.'
 
 if len(sys.argv) < 2:
-    print("Usage: "+sys.argv[0]+" path")
+    print("Usage: " + sys.argv[0] + " path")
     sys.exit(0)
 
 try:
-    log = open(logfile, 'a')
+    log = open(args.log_file, 'a')
 except:
-    print("Can't open log file for writing ("+logfile+")")
+    print("Can't open log file for writing (" + args.log_file + ")")
     sys.exit()
 
 logit("New run")
-for root, dirs, files in os.walk(sdcard):
+for root, dirs, files in os.walk(args.pictures + '/'):
 
     # exclude dirs
     dirs[:] = [os.path.join(root, d) for d in dirs]
@@ -125,7 +189,7 @@ for root, dirs, files in os.walk(sdcard):
         try:
             resp = flickr.upload(filename=fname, tags='rpiup', is_public=0)
             pre, ext = os.path.splitext(fname)
-            os.rename(fname, pre + done)
+            os.rename(fname, pre + '.' + args.uploaded_extension)
             logit(fname + " : " + resp.find('photoid').text)
         except:
             logit("Error on " + fname)
